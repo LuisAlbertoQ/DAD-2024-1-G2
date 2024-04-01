e0ab1b97c649ae0a3fe14f53be5b572cc40b4e1f
LuisAlbertoQ <quillaluis501@gmail.com>
2024-04-01 09:32:36 -0500

    eureka

---
+++ Msregistry-server/src/main/java/com/example/msregistryserver/MsregistryServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.msregistryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsregistryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsregistryServerApplication.class, args);
+	}
+
+}
